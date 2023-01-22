8bb2d6450dda198fdda56b0673b9c4e18d67be22
Paulo Victor <paulovictor55296@gmail.com>
2023-01-22 10:50:08 -0300

    para fixar 04

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/para-fixar-04.js
@@ -0,0 +1,28 @@
+const currentHour = 3.59;
+let message = "";
+// Se o horário for maior ou igual a 22, insira “Não deveríamos comer nada, é hora de dormir” na variável “message”.
+
+// Se o horário for maior ou igual a 18 e menor que 22, insira “Rango da noite, vamos jantar :D” na variável “message”.
+
+// Se o horário for maior ou igual a 14 e menor que 18, insira “Vamos fazer um bolo pro café da tarde?” na variável “message”.
+
+// Se o horário for maior ou igual a 11 e menor que 14, insira “Hora do almoço!!!” na variável “message”.
+
+// Se o horário estiver entre 4 e 11, insira “Hmmm, cheiro de café recém-passado” na variável “message”.
+
+
+if (currentHour >=18 && currentHour <= 22) {
+    console.log('Rango da noite, vamos jantar :D');
+}
+else if (currentHour >= 14 && currentHour < 18) {
+    console.log('Vamos fazer um bolo pro café da tarde?');
+}
+else if (currentHour >= 11 && currentHour < 14) {
+    console.log('Hora do almoço!!!');
+}
+else if (currentHour >= 4 && currentHour < 11) {
+    console.log('Hmmm, cheiro de café recém-passado');
+}
+else {
+    console.log('Não deveríamos comer nada, é hora de dormir');
+}
