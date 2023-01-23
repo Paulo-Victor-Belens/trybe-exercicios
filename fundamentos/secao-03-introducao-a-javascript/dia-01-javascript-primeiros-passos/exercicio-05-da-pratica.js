d938e683c90e59fed817ee16c4051ed1d2ea9b48
Paulo Victor <paulovictor55296@gmail.com>
2023-01-23 10:52:13 -0300

    exercicio 05 da pratica

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-05-da-pratica.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-05-da-pratica.js
@@ -1,11 +1,20 @@
 const angulo01 = 70;
-const angulo02 = 60;
+const angulo02 = 70;
 const angulo03 = 40;
 let anguloValido = true
 
-if ((angulo01 + angulo02 + angulo03) === 180) {
-    console.log(anguloValido);
+if (angulo01 <= 0 || angulo02 <= 0 || angulo03 <= 0){
+    console.log('Inválido,valores devem ser maiores que 0');
 }
+
+else if ((angulo01 + angulo02 + angulo03) === 180) {
+    console.log(anguloValido + ', É um ângulo válido');
+}
+
+else if ((angulo01 + angulo02 + angulo03) < 180){
+    console.log(!anguloValido + ', Não é um ângulo válido');
+}
+
 else {
-    console.log(!anguloValido);
+    console.log('Não é um Triângulo!!!');
 }
