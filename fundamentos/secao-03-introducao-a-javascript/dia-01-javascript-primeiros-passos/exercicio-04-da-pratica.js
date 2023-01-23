82a6ed57de231a2c13ebcdc06a5b111b414c370d
Paulo Victor <paulovictor55296@gmail.com>
2023-01-23 09:10:56 -0300

    exercicio 04 da pratica

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-04-da-pratica.js
@@ -0,0 +1,11 @@
+let parametro = 0;
+
+if (parametro > 0) {
+    console.log('Núemro positivo');
+}
+else if (parametro < 0) {
+    console.log('Número negativo');
+}
+else {
+    console.log('Número igual a zero');
+}
