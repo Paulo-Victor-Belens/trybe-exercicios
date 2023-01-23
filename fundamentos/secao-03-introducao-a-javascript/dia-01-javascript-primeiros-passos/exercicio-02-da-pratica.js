5c1c04e4a28a89ec2afa12aac096019f8c326876
Paulo Victor <paulovictor55296@gmail.com>
2023-01-23 08:57:18 -0300

    exercicios da prática

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-02-da-pratica.js
@@ -0,0 +1,12 @@
+const a = 13;
+const b = 13;
+
+if (a > b) {
+    console.log('a é maior que b');
+}
+else if (a < b) {
+    console.log('b é maior que a');
+}
+else {
+    console.log('a e b são iguais')
+}
