36842ca6da638a138b27842d8148c52b8c3c74c7
Paulo Victor Belens Moreira <paulovictor55296@gmail.com>
2023-04-03 15:00:32 -0300

    go go power ranger

--- front-end/secao-03-dia-01/fancy-buttons/src/App.js
+++ front-end/secao-03-dia-01/fancy-buttons/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import './App.css';
 
-const actionBtn1 = () => {
-  console.log('actionBtn1');
-};
 
-const actionBtn2= () => {
-  console.log('actionBtn2');
-};
-
-const actionBtn3 = () => {
-  console.log('actionBtn3');
-};
-
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.actionBtn1 = this.actionBtn1.bind(this);
+    this.actionBtn1 = this.actionBtn2.bind(this);
+    this.actionBtn1 = this.actionBtn3.bind(this);
+  }
+  actionBtn1 = () => {
+    console.log('actionBtn1');
+  };
+  
+  actionBtn2 = () => {
+    console.log('actionBtn2');
+  };
+  
+  actionBtn3 = () => {
+    console.log(this);
+    console.log('actionBtn3');
+  };
+  render() {
   return (
     <section className="container-buttons">
-      <button className="btn1" onClick={actionBtn1}>1</button>
-      <button className="btn2" onClick={actionBtn2}>2</button>
-      <button className="btn3" onClick={actionBtn3}>3</button>
+      <button className="btn1" onClick={this.actionBtn1}>1</button>
+      <button className="btn2" onClick={this.actionBtn2}>2</button>
+      <button className="btn3" onClick={this.actionBtn3}>3</button>
     </section>
   );
+}
 }
 
 export default App;
